574f70e4eb82f411c38eb07f4ccaabd6a0c965ce
Travis Gallagher <92351358+travisgallagher@users.noreply.github.com>
2022-02-23 16:10:50 -0700

    core lab completed

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 console.log(`Hello World`);
 
-// Grabbing the necessary buttons. 
-document.querySelector("form")
+const message = document.querySelector("#message"); 
 
+document.querySelector("form").addEventListener("submit", addMovie); 
 
 
 function addMovie(event) {
+    event.preventDefault(); 
 
-    const inputField = document.querySelector("#input-field"); 
+    const inputField = document.querySelector("input"); 
 
     const movie = document.createElement("li");
 
     const movieTitle = document.createElement("span");
+
+    addEventListener("click", crossOffMovie); 
+
     movieTitle.textContent = inputField.value; 
-    movie.appendChild(movieTitle); 
+    movie.appendChild(movieTitle);
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x"; 
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn); 
 
     const list = document.querySelector("ul");
     list.appendChild(movie); 
+
+    inputField.value = ""; 
+};
+
+
+function deleteMovie (event) {
+    message.textContent = "Movie Deleted!"
+    event.target.parentNode.remove(); 
 }
 
 
+function crossOffMovie(event) {
 
+    event.target.classList.toggle("checked"); 
 
-
-
-
-// Add event listener to my elements. 
+    if (event.target.classList.contains("checked") === true) {
+        message.textContent = "Movie watched!"
+    } else {
+        message.textContent = "Movie added back!"
+    }
+}
